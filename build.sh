b47bcff725986fca22b4741dd739352a3d5f4c49
Khash Sajadi <khash@cloud66.com>
2019-04-10 15:13:24 -0700

    list the versions first
    Signed-off-by: Khash Sajadi <khash@cloud66.com>

--- build.sh
+++ build.sh
@@ -11,7 +11,12 @@ fi
 channel=$1
 
 echo "Building $channel/$version"
+echo
 
 rm build/*
 curl -s http://downloads.cloud66.com.s3.amazonaws.com/trackman/versions.json | jq '.versions |= map(if (.channel == "'$channel'") then .version = "'$version'" else . end)' > build/versions.json
+echo "Current Versions"
+cat build/versions.json | jq -r '.versions | map([.channel, .version] | join(": ")) | .[]'
+echo
+
 gox -ldflags "-X github.com/cloud66/trackman/utils.Version=$version -X github.com/cloud66/trackman/utils.Channel=$channel" -os="darwin linux windows" -arch="amd64" -output "build/{{.OS}}_{{.Arch}}_$version"
