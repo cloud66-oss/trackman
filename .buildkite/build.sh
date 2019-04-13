7325afdc8887105ffc9b55703f5e7b925708a4ce
Khash Sajadi <khash@cloud66.com>
2019-04-13 15:54:38 +0100

    upload artefacts to GH
    Signed-off-by: Khash Sajadi <khash@cloud66.com>

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -26,8 +26,9 @@ echo
 
 echo "Building"
 
-docker run -i --rm -w /gopath/src/github.com/cloud66/trackman -v $(pwd):/gopath/src/github.com/cloud66/trackman cloud66/gobuildchain /bin/bash << COMMANDS
+docker run -i -e GITHUB_TOKEN=$GITHUB_TOKEN --rm -w /gopath/src/github.com/cloud66/trackman -v $(pwd):/gopath/src/github.com/cloud66/trackman cloud66/gobuildchain:2 /bin/bash << COMMANDS
 gox -ldflags "-X github.com/cloud66/trackman/utils.Version=$version -X github.com/cloud66/trackman/utils.Channel=$channel" -os="darwin linux windows" -arch="amd64" -output "build/{{.OS}}_{{.Arch}}_$version"
+ghr -soft $version build/
 chown -R 999:998 build
 COMMANDS
 
