adfb93b773a5f36b34599b2781d3a6a45e9a8e3e
Khash Sajadi <khash@cloud66.com>
2019-04-13 12:54:58 +0100

    affing specs
    Signed-off-by: Khash Sajadi <khash@cloud66.com>

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -15,7 +15,16 @@ if [[ $BUILDKITE_BRANCH -eq "master" ]]
     channel="stable"
 fi
 
+echo "Building $channel/$version"
+
 mkdir build
+
+curl -s http://downloads.cloud66.com.s3.amazonaws.com/trackman/versions.json | jq '.versions |= map(if (.channel == "'$channel'") then .version = "'$version'" else . end) | .versions |= map(if (.channel == "'$channel'") then .force = '$force' else . end)' > build/versions.json
+echo "Current Versions"
+cat build/versions.json | jq -r '.versions | map([.channel, .version] | join(": ")) | .[]'
+echo
+
+echo "Building"
 
 docker run -i --rm -w /gopath/src/github.com/cloud66/trackman -v $(pwd):/gopath/src/github.com/cloud66/trackman cloud66/gobuildchain /bin/bash << COMMANDS
 gox -ldflags "-X github.com/cloud66/trackman/utils.Version=$version -X github.com/cloud66/trackman/utils.Channel=$channel" -os="darwin linux windows" -arch="amd64" -output "build/{{.OS}}_{{.Arch}}_$version"
